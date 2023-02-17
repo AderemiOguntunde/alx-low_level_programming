72e79ada6c3febd4c31fa1a8f9169d531fdb72ef
aderemi <aderemioguntunde@gmail.com>
2023-02-17 03:30:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main- a program that print a line with put function
  * Return:0 (success)
- **/
+ */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main- write content in a line that begins with a new lin3
+ *Return :0 (success0
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
