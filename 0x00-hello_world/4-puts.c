9351372676c1cb748d34e7d38bbac5bb1a00e75d
aderemi <aderemioguntunde@gmail.com>
2023-02-16 20:21:05 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- a program that print a line with put function
+ * Return:0 (success)
+ **/
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
