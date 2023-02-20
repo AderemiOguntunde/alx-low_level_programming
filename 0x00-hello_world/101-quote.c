a56a2159116ae6bc75d6cb1d5e5f46c8697b5a33
aderemi <aderemioguntunde@gmail.com>
2023-02-20 04:59:06 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that print out without prinf or put function
+ * main - A C program that print out without prinf or put functn
  * Return: 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
 	return (1);
 }
