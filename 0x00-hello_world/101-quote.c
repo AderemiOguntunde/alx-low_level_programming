1c400a70257c8ae14c211b8deb83e05ea9b13ce5
aderemi <aderemioguntunde@gmail.com>
2023-02-20 06:15:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+char str[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, str, sizeof(str - 1);
 	return (1);
 }
