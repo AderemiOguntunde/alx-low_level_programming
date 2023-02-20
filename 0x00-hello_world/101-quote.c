416955d4f79b66606c59e5be6ea211c6c421fa87
aderemi <aderemioguntunde@gmail.com>
2023-02-20 15:04:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
- * main - A C program that print out without prinf or put functn
- * Return: 0 (Success)
+ * main - Entry point
+ * Return: 1 (Success)
  */
 int main(void)
 {
-char str[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(STDOUT_FILENO, str, sizeof(str - 1);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
