aeed4fdeab819e00861ef535b7cd686f6c480954
aderemi <aderemioguntunde@gmail.com>
2023-02-20 17:18:00 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabetic
+ * Return: A;ways (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
